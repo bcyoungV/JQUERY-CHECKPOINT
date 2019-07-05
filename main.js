975e8314a1da46ecc1d8b286576fa65c0f9251da
bcyoungV <52163618+bcyoungV@users.noreply.github.com>
2019-07-05 12:29:49 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,111 @@
+/*Test du cours... */
+
+/* function B(){
+    document.getElementById("textarea1").style.fontWeight="bold";
+}
+function I(){
+    document.getElementById("textarea1").style.fontStyle="italic";
+}
+
+function U(){
+    document.getElementById("textarea1").style.textDecoration="underline";
+}
+
+var x=document.getElementById("textarea1");
+function px()
+{
+    x.style.fontSize=document.getElementById("select").value;
+}
+function font()
+{
+    x.style.fontFamily=document.getElementById("fs").value;
+}
+
+$(document).ready(function(){
+    $("#hide").click(function(){
+        $("p").hide();
+    });
+    $("#show").click(function(){
+        $("p").show();
+    });
+});
+
+
+$(document).ready(function(){
+    $("button").click(function(){
+        $("#ppp").toggle();
+    });
+});
+
+
+
+
+
+
+$("button").click(function(){
+    $("#first").fadeOut();
+    $("#second").fadeOut("slow");
+    $("#third").fadeOut("3000");
+});
+
+
+/* Ce code concerne le Checkpoints pour JQuery */
+
+$("#bby1").mouseover(function(){
+    $("#btn1").show();
+    $("#bby1").css("opacity","0.7");
+});
+
+$("#bby1").mouseleave(function(){
+    $("#btn1").hide();
+    $("#bby1").css("opacity","1");
+});
+
+
+$("#bby2").mouseover(function(){
+    $("#btn2").show();
+    $("#bby2").css("opacity","0.7");
+});
+
+$("#bby2").mouseleave(function(){
+    $("#btn2").hide();
+    $("#bby2").css("opacity","1");
+});
+
+$("#bby3").mouseover(function(){
+    $("#btn3").show();
+    $("#bby3").css("opacity","0.7");
+});
+
+$("#bby3").mouseleave(function(){
+    $("#btn3").hide();
+    $("#bby3").css("opacity","1");
+});
+
+$("#bby4").mouseover(function(){
+    $("#btn4").show();
+    $("#bby4").css("opacity","0.7");
+});
+$("#bby4").mouseleave(function(){
+    $("#btn4").hide();
+    $("#bby4").css("opacity","1");
+});
+
+$("#bby5").mouseover(function(){
+    $("#btn5").show();
+    $("#bby5").css("opacity","0.7");
+});
+$("#bby5").mouseleave(function(){
+    $("#btn5").hide();
+    $("#bby5").css("opacity","1");
+});
+
+$("#bby6").mouseover(function(){
+    $("#btn6").show();
+    $("#bby6").css("opacity","0.7");
+});
+$("#bby6").mouseleave(function(){
+    $("#btn6").hide();
+    $("#bby6").css("opacity","1");
+});
+
